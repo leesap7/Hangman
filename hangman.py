4136f153c41bc78116c8639664eff51d046810c9
leesap7 <110601500+leesap7@users.noreply.github.com>
2023-02-03 17:17:52 +1100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,6 +1,9 @@
 #simple hangman game with one secret word
 
-print("Try to guess the secret word before the hangman picture us complete.\n You can guess one letter at a time.\n If you guess a letter right, you will see the letter printed on the screen in the correct position/s in the word.\n If you guess wrong, an element will be added to the picture.\n")
+print("Try to guess the secret word before the hangman picture us complete.\n" 
+      "You can guess one letter at a time.\n"
+      "If you guess a letter right, you will see the letter printed on the screen in the correct"
+      "position/s in the word.\nIf you guess wrong, an element will be added to the picture.\n")
 secret_word = ("sunflower")
 guess_counter = 1
 lives = 9
